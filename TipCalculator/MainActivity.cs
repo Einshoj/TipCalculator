ea38b9620ef14e33939472bf963128273f488ce2
Einshoj <alexander.einshoj@gmail.com>
2021-07-06 16:07:35 +0200

    Finished TipCalculator project. Note: Android.Widget is necessary in MainActivity.cs in order to declare and use the UI widgets like 'Button', 'EditText' etc

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -5,23 +5,49 @@ using Android.Runtime;
 using Android.Views;
 using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
-using Google.Android.Material.FloatingActionButton;
-using Google.Android.Material.Snackbar;
+using Android.Widget;
+
 
 namespace TipCalculator
 {
+
+    
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
 
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            calculateButton.Click += OnCalculateClick;
+
+        }
+
+        void OnCalculateClick(object sendcer, EventArgs e)
+        {
+            string text = inputBill.Text;
+            double bill = 0;
+            if (double.TryParse(text, out bill))
+            {
+                var tip = bill * 0.15;
+                var total = bill + tip;
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString(); 
+            }
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
